a8752bcdee63f350d7dd809161b9a7524f6c1976
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-03-05 11:10:45 +0200

    Disjoint Sets simple algorithm

---
+++ Disjoint Set/src/DisjSets.java
@@ -0,0 +1,42 @@
+import java.util.Vector;
+
+public class DisjSets {
+
+	private int[] S;
+	
+	DisjSets(int numElements)
+	{
+		S=new int[numElements];
+		makeset(numElements);
+	}
+	
+	private void makeset(int n)
+	{
+		for(int i=0;i<n;++i)
+			S[i]=-1;
+	}
+	
+	void unionSets(int root1,int root2)
+	{
+		S[root2]=root1;
+	}
+	
+	int find(int x)
+	{
+		if(S[x]<0)
+			return x;
+		else 
+		return find(S[x]);
+	}
+	
+	public static void main(String[] args) {
+		DisjSets d=new DisjSets(5);
+		d.unionSets(0, 3);
+		d.unionSets(3, 1);
+		d.unionSets(2, 4);
+		System.out.println(d.find(4));
+		
+
+	}
+
+}
